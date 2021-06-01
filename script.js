3adc5556df8945e3c382ab3b8d4beb1065ef0bdf
mateusluiz <mateus74890@gmail.com>
2021-06-01 04:26:45 -0300

    Adiciona nova funcionalidades de mudança de tema. Ainda em progresso...

--- script.js
+++ script.js
@@ -43,10 +43,35 @@ function calcular() {
 function calcularComEnter() {
   addEventListener('keydown', function(event) {
     if(event.key === 'Enter') {
-      document.activeElement.blur() // Remove foco do Enter
+      document.activeElement.blur() // Remove foco do botão antes de pressionar o enter
       if(visor.value) {
         visor.value = eval(visor.value)
       }
     }
   })
 }
+
+
+const tema1 = document.getElementById('tema-1')
+const tema2 = document.getElementById('tema-2')
+const tema3 = document.getElementById('tema-3')
+const fundo = document.querySelector('.visor')
+
+tema1.addEventListener('change', (e) => {
+  fundo.classList.remove('tema-2')
+  fundo.classList.remove('tema-3')
+  fundo.classList.toggle('tema-1', e.target.checked)
+})
+
+tema2.addEventListener('change', (e) => {
+  fundo.classList.remove('tema-1')
+  fundo.classList.remove('tema-3')
+  fundo.classList.toggle('tema-2', e.target.checked)
+})
+
+tema3.addEventListener('change', (e) => {
+  fundo.classList.remove('tema-1')
+  fundo.classList.remove('tema-2')
+  fundo.classList.toggle('tema-3', e.target.checked)
+})
+
